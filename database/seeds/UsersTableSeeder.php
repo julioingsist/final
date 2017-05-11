a31394985435f4d0455602dba13fbefec54c4d0e
julioingsist <julioingsist@gmail.com>
2017-05-11 00:20:35 -0600

    se modifico seeder de usuario

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,25 +1,23 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
 
 class UsersTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      *
+  use App\User;
      * @return void
      */
     public function run()
     {
-    	$fecha = new \DateTime();
-	    $fecha->format('Y-m-d H:m:s');
-
-	    DB::table('users')->insert([
+        User::insert([
     		'usuario' => "admin",
     		'email' => "julioingsist@gmail.com",
     		'password' => bcrypt("199425julio"),
     		'tipo' => 1,
-    		'created_at' => $fecha,
     		]);	
     }
-}
+}        
